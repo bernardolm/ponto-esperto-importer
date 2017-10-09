2071c45d116b353d3dc5ed1c3c5ae4ccc955bff0
Bernardo Loureiro <bernardo.lou@gmail.com>
2017-10-09 19:47:25 -0300

    Testing first item

--- importer/main_test.go
+++ importer/main_test.go
@@ -7,8 +7,10 @@ import (
 )
 
 func TestDo(t *testing.T) {
-	actual := Do("../file.csv")
+	actual := Do("../timesheet.csv")
 
 	assert.NotNil(t, actual)
-	assert.Len(t, actual, 3)
+	assert.Len(t, actual, 116)
+
+	assert.Equal(t, "03/08/15", actual[0].Date)
 }
