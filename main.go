9df4275bbfd0b89ff339fcf8b5e752217b55655f
Bernardo Loureiro <bernardo.lou@gmail.com>
2017-10-09 20:34:34 -0300

    Implementing driver

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strings"
 
 	"github.com/bernardolm/ponto-esperto-importer/importer"
+	"github.com/bernardolm/ponto-esperto-importer/meuponto"
 )
 
 func main() {
@@ -13,10 +15,23 @@ func main() {
 	var file string
 	flag.StringVar(&file, "file", "timesheet.csv", "file path with timesheet entries exported by Ponto Esperto app")
 
+	var driver string
+	flag.StringVar(&driver, "driver", "meuponto", "driver to transform importer output")
+
 	var debug bool
 	flag.BoolVar(&debug, "debug", false, "flag to print debug infos")
 
 	flag.Parse()
 
-	importer.Do(file, debug)
+	workdays := importer.Do(file, debug)
+
+	switch driver {
+	default:
+		filePathParts := strings.Split(file, ".")
+		if len(filePathParts) == 0 {
+			panic("unknown output file")
+		}
+		filePath := fmt.Sprintf("%s_result.json", filePathParts[0])
+		meuponto.Do(workdays, filePath, debug)
+	}
 }
--- meuponto/main.go
+++ meuponto/main.go
@@ -1,1 +1,7 @@
 package meuponto
+
+import (
+	"github.com/bernardolm/ponto-esperto-importer/importer"
+)
+
+func Do(workdays []importer.Workday, filePath string, debug bool) {}
