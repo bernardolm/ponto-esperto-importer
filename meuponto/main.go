6e25d1b32e25d92446c4e80cf147745ad680fd28
Bernardo Loureiro <bernardo.lou@gmail.com>
2017-10-09 21:39:34 -0300

    Adding time parsers

--- meuponto/main.go
+++ meuponto/main.go
@@ -1,7 +1,53 @@
 package meuponto
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/bernardolm/ponto-esperto-importer/importer"
 )
 
 func Do(workdays []importer.Workday, filePath string, debug bool) {}
+
+func parseDate(date string) *time.Time {
+	d, err := time.Parse("02/01/06", date)
+	if err != nil {
+		fmt.Printf("\n\n err %+v \n\n", err)
+		return nil
+	}
+	return &d
+}
+
+func parseTime(hourMinute string) *time.Time {
+	t, err := time.Parse("15:04", hourMinute)
+	if err != nil {
+		fmt.Printf("\n\n err %+v \n\n", err)
+		return nil
+	}
+	return &t
+}
+
+func mergeDateTime(dateString string, timeString string) *time.Time {
+	if len(dateString) == 0 {
+		return nil
+	}
+
+	if len(timeString) == 0 {
+		return nil
+	}
+
+	date := parseDate(dateString)
+	if date == nil {
+		return nil
+	}
+
+	hourMinute := parseTime(timeString)
+	if hourMinute == nil {
+		return nil
+	}
+
+	y, m, d := date.Date()
+	result := time.Date(y, m, d, hourMinute.Hour(), hourMinute.Minute(), 0, 0, time.UTC)
+
+	return &result
+}
---
+++ meuponto/main_test.go
@@ -0,0 +1,80 @@
+package meuponto
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseDate(t *testing.T) {
+	actual := parseDate("03/08/999")
+	assert.Nil(t, actual)
+
+	actual = parseDate("31/13/99")
+	assert.Nil(t, actual)
+
+	actual = parseDate("32/12/99")
+	assert.Nil(t, actual)
+
+	actual = parseDate("03/08/15")
+	assert.NotNil(t, actual)
+	assert.Equal(t, 2015, actual.Year())
+	assert.Equal(t, 8, int(actual.Month()))
+	assert.Equal(t, 3, actual.Day())
+
+	actual = parseDate("31/12/99")
+	assert.NotNil(t, actual)
+	assert.Equal(t, 1999, actual.Year())
+	assert.Equal(t, 12, int(actual.Month()))
+	assert.Equal(t, 31, actual.Day())
+}
+
+func TestParseTime(t *testing.T) {
+	actual := parseTime("25:00")
+	assert.Nil(t, actual)
+
+	actual = parseTime("00:60")
+	assert.Nil(t, actual)
+
+	actual = parseTime("00:00")
+	assert.NotNil(t, actual)
+	assert.Equal(t, 0, actual.Hour())
+	assert.Equal(t, 0, actual.Minute())
+
+	actual = parseTime("23:59")
+	assert.NotNil(t, actual)
+	assert.Equal(t, 23, actual.Hour())
+	assert.Equal(t, 59, actual.Minute())
+
+	actual = parseTime("12:12")
+	assert.NotNil(t, actual)
+	assert.Equal(t, 12, actual.Hour())
+	assert.Equal(t, 12, actual.Minute())
+}
+
+func TestMergeDateTime(t *testing.T) {
+	actual := mergeDateTime("", "")
+	assert.Nil(t, actual)
+
+	actual = mergeDateTime("31/12/99", "")
+	assert.Nil(t, actual)
+
+	actual = mergeDateTime("", "23:59")
+	assert.Nil(t, actual)
+
+	actual = mergeDateTime("31/12/99", "23:59")
+	assert.NotNil(t, actual)
+	assert.Equal(t, 1999, actual.Year())
+	assert.Equal(t, 12, int(actual.Month()))
+	assert.Equal(t, 31, actual.Day())
+	assert.Equal(t, 23, actual.Hour())
+	assert.Equal(t, 59, actual.Minute())
+
+	actual = mergeDateTime("01/01/00", "00:00")
+	assert.NotNil(t, actual)
+	assert.Equal(t, 2000, actual.Year())
+	assert.Equal(t, 1, int(actual.Month()))
+	assert.Equal(t, 1, actual.Day())
+	assert.Equal(t, 0, actual.Hour())
+	assert.Equal(t, 0, actual.Minute())
+}
